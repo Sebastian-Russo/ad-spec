a03d79855c9f8a75145881ac6aa5a94246179879
Sebastian Russo <russo.sebastian@gmail.com>
2020-10-27 17:35:59 -0400

    nav colors

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './navbar.css';
 
 class Navbar extends Component {
   render() { 
     return ( 
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <Link to="/" className="navbar-brand" ><img src="images/ASI-logo.jpeg" alt="logo" width="50%"/></Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -13,28 +14,28 @@ class Navbar extends Component {
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
+              <li className="nav-item">
                 <Link to="/" className="nav-link" >Home<span className="sr-only"></span></Link>
               </li>
-              <li className="nav-item active">
+              <li className="nav-item">
                 <Link to="about" className="nav-link" >About<span className="sr-only"></span></Link>
               </li>
-              <li className="nav-item active">
+              <li className="nav-item">
                 <Link to="hotmelt" className="nav-link" >Hot Melt<span className="sr-only"></span></Link>
               </li>
-              <li className="nav-item active">
+              <li className="nav-item">
                 <Link to="pressure-sensitive" className="nav-link" >Pressure Sensitive<span className="sr-only"></span></Link>
               </li>
-              <li className="nav-item active">
+              <li className="nav-item">
                 <Link to="liquid" className="nav-link" >Liquid<span className="sr-only"></span></Link>
               </li>
-              <li className="nav-item active">
+              <li className="nav-item">
                 <Link to="equipment" className="nav-link" >Equipment<span className="sr-only"></span></Link>
               </li>
-              <li className="nav-item active">
+              <li className="nav-item">
                 <Link to="careers" className="nav-link" >Careers<span className="sr-only"></span></Link>
               </li>
-              <li className="nav-item active">
+              <li className="nav-item">
                 <Link to="contact" className="nav-link" >Contact<span className="sr-only"></span></Link>
               </li>
             </ul>
