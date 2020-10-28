913870ffd99267e1a1cde7ecda0f67ebe23322f2
Sebastian Russo <russo.sebastian@gmail.com>
2020-10-27 21:13:16 -0400

    updated footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,20 +4,39 @@ import './footer.css';
 
 const Footer = () => {
   return ( 
-    <div className="footer">
-      <div>
-        <Link to="contact" className="footer-contact">Contact Us</Link>
-      </div>
-      <div>
+    <div className="container-footer">
+      <div className="row">
+        <div className="col">
+          <Link to="/" className="footer-link" ><img src="images/ASI-logo.jpeg" alt="logo" width="110px" className="logo"/></Link>
+        </div>
+        <div className="col">
+          <ul>
+            <li><Link to="contact" className="footer-link">Contact Us</Link></li>
+            <li><Link to="/" className="footer-link" >Home</Link></li>
+            <li><Link to="about" className="footer-link" >About</Link></li>
+            <li><Link to="hotmelt" className="footer-link" >Hot Melt</Link></li>
+            <li><Link to="pressure-sensitive" className="footer-link" >Pressure Sensitive</Link></li>
+            <li><Link to="liquid" className="footer-link" >Liquid</Link></li>
+            <li><Link to="equipment" className="footer-link" >Equipment</Link></li>
+            <li><Link to="careers" className="footer-link" >Careers</Link></li>
+          </ul>
+        </div>
+        <div className="col">
+          <h3><a href="mailto:stever@adhesivesspecialists.com" className="footer-link request">Request a price quote</a></h3>
+          <h3><a href="mailto:stever@adhesivesspecialists.com" className="footer-link request">Request an adhesive appraisal</a></h3>
+        </div>
+        <div className="col">
+          <p>Family owned, Established in 1983</p>
+          <p>Adhesives Specialists Inc.</p>
+          <p>739 Roble Road Allentown, PA 18109</p>
+          <p>Phone: 1-800-275-7006</p>
           <a href="https://www.linkedin.com/company/adhesives-specialists/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin fa-lg m-2" style={{color: "#FFF"}}></i></a>
           <a href="https://www.facebook.com/adhesivesspecialistsinc" target="_blank" rel="noreferrer"><i className="fab fa-facebook-square fa-lg m-1" style={{color: "#FFF"}}></i></a>
           <a href="mailto:customerservice@adspecinc.com"><i className="fas fa-envelope-square fa-lg m-1" style={{color: "#FFF"}}></i></a>
-      </div>
-      <div>
-        Adhesives Specialists Inc., Family owned, Established in 1983
+        </div>
       </div>
     </div>
   );
 }
- 
+
 export default Footer;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ class Navbar extends Component {
   render() { 
     return ( 
       <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg ">
           <Link to="/" className="navbar-brand" ><img src="images/ASI-logo.jpeg" alt="logo" width="50%"/></Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
